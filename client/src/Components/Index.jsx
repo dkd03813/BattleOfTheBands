8c5d237853a550a784f21d751d476c37161dd6a1
Damian Diaz <damiandiaz0509@gmail.com>
2023-08-31 21:49:58 -0400

    added some react components and started some routing for backend

---
+++ client/src/Components/Index.jsx
@@ -0,0 +1,14 @@
+export default function Index() {
+    //This will serve as the default page of the application, holding the name of the game and a create account and login button
+    return (
+      <p id="zero-state">
+        This is a demo for React Router.
+        <br />
+        Check out{" "}
+        <a href="https://reactrouter.com">
+          the docs at reactrouter.com
+        </a>
+        .
+      </p>
+    );
+  }
