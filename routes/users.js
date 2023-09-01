4731d2441c53ae9cd6536425cc5a5d466d8c2e04
Damian Diaz <damiandiaz0509@gmail.com>
2023-08-31 21:51:42 -0400

    pushing up changes to the backend routes folder

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+router.post('/register', async (req, res) => {
+  try {
+    // Handle user registration logic here
+    // Example: Create a new user record in the database
+    const newUser = await User.create(req.body);
+    res.json({ message: 'User registered successfully', user: newUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+// POST route for user login
+router.post('/login', async (req, res) => {
+  try {
+    // Handle user authentication logic here
+    // Example: Check user credentials and return a response
+    // You might use sessions, JWT, or other authentication mechanisms
+    res.json({ message: 'Login successful' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
 module.exports = router;
