33ce9c4336d16023388db3a2de23c1ac8b9a3bbf
Damian Diaz <damiandiaz0509@gmail.com>
2023-09-03 22:05:41 -0400

    finished creating login functionality of the site

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,25 +1,87 @@
-// The component for displaying a form that has a username field and a password field
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  const userLogin = async (e) => {
+    e.preventDefault();
+  
+    try {
+      console.log("in the try");
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+  
+      // Log the response status
+      console.log('Response status:', response.status);
+  
+      // Parse the JSON response
+      const data = await response.json();
+      console.log('Response data:', data);
+  
+      // Check if the request was successful
+      if (response.ok) {
+        // Check if login was successful
+        if (data.success) {
+          console.log('User successfully logged in');
+          // Redirect to the home page on successful login
+          navigate('/');
+        } else {
+          console.log('User login failed');
+        }
+      } else {
+        console.error('POST request failed'); // Log an error if the request fails
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
+  
+
   return (
     <div>
       <h1>Login</h1>
       <form>
         <div>
           <label htmlFor="username">Username:</label>
-          <input type="text" id="username" placeholder="Username" />
+          <input
+            type="text"
+            id="username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" placeholder="Password" />
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <button type="submit">Enter</button>
+        <button type="submit" onClick={userLogin}>
+          Enter
+        </button>
         <Link to="/">
-            <button>Back</button>
-          </Link>
+          <button>Back</button>
+        </Link>
       </form>
     </div>
   );
 }
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const fs = require("fs");
 const jwt = require("jsonwebtoken");
 const saltRounds = 10;
 const { User } = require("../models");
-
-
+const findProfile = require("../middleware/findProfile");
+const authCheck = require("../middleware/authCheck");
 
 router.post("/create", async (req, res) => {
   console.log("Received POST request to /api/create");
@@ -27,15 +27,49 @@ router.post("/create", async (req, res) => {
     console.log("New User's auto-generated ID:", newProfile.id);
 
     // Send a success response
-    res.status(201).json({ message: 'User created successfully' });
+    res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     console.log(error);
 
     // Send an error response
-    res.status(500).json({ message: 'User creation failed' });
+    res.status(500).json({ message: "User creation failed" });
   }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
+    const user = await User.findOne({ where: { username } });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const hashPassword = user.password;
+    const userID = user.id;
+
+    
+    const result = await bcrypt.compare(password, hashPassword);
+
+    if (result) {
+      const token = jwt.sign({ foo: userID }, "superSecretPrivateKey", {
+        expiresIn: "1h",
+      });
+      console.log("Password:", password);
+      console.log("Hashed Password:", hashPassword);
+      console.log("Comparison Result:", result);
+      console.log("Token:", token);
+      res.cookie("token", token);
+      return res.status(200).json({ success: true, message: "User successfully logged in" });
+    } else {
+      console.log("User couldn't login");
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
 
 module.exports = router;
