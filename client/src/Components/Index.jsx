fe77078a8349bf244fa6a40e5e92a724f6ce43c9
Erica Bitting <ericabitting@gmail.com>
2023-09-05 21:33:19 -0400

    added Tailwind, I am still editing the design

--- client/src/Components/Index.jsx
+++ client/src/Components/Index.jsx
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 export default function Index() {
     //This will serve as the default page of the application, holding the name of the game and a create account and login butto
       return (
-        <div>
-          <h1>Welcome to our App</h1>
-          <Link to="/create">
+        <div className="group">
+          <section className="h-screen bg-orange-100 flex items-center justify-center m-12 text-center text-3xl text-cyan-600">
+          <h1>Welcome to our Oregon-Trail inspired Game!</h1>
+          <Link to="/create"> 
             <button>Create Account</button>
-          </Link>
+          </Link> 
           <Link to="/login">
             <button>Login</button>
-          </Link>
+          </Link>  
+          </section>
         </div>
       );
 
